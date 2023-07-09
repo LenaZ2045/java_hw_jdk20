af95994a62eb7af26621dfd7b66efafc32b11a51
Lena Zimin <Bogypesn@gmail.com>
2023-07-08 22:42:45 -0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        String a = "THIS ";
+        String b = "IS ";
+        String c = "HOMEWORK ";
+
+        int one = 2023;
+        int two = 7;
+        int three = 5;
+
+        System.out.println(a + b + c + "OF " + three + '/' + two + "/" + one);
+    }
+}
