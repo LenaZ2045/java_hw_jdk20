9bb71c398d4033e468fa24fd64c26d870e302cac
Lena Zimin <Bogypesn@gmail.com>
2023-07-11 16:40:15 -0700

    weather forecast

---
+++ src/portland_weather/Main.java
@@ -0,0 +1,39 @@
+package portland_weather;
+
+public class Main {
+    public static void main(String[] args) {
+        WeeklyForecast weather = new WeeklyForecast();
+        System.out.println();
+        System.out.println("USA WEEKLY WEATHER FORECAST in Portland, OR");
+        System.out.println();
+        System.out.println(weather.weekDay1);
+        System.out.println("High temperature is: " + weather.tempMonday + "'F");
+        System.out.println("Day will be: " + weather.conditionOfDay3);
+        System.out.println();
+        System.out.println(weather.weekDay2);
+        System.out.println("High temperature is: " + weather.tempTuesday + "'F");
+        System.out.println("Day will be: " + weather.conditionOfDay1);
+        System.out.println();
+        System.out.println(weather.weekDay3);
+        System.out.println("High temperature is: " + weather.tempWednesday + "'F");
+        System.out.println("Day will be: " + weather.conditionOfDay2);
+        System.out.println();
+        System.out.println(weather.weekDay4);
+        System.out.println("High temperature is: " + weather.tempThursday + "'F");
+        System.out.println("Day will be: " + weather.conditionOfDay1);
+        System.out.println();
+        System.out.println(weather.weekDay5);
+        System.out.println("High temperature is: " + weather.tempFriday + "'F");
+        System.out.println("Day will be: " + weather.conditionOfDay1);
+        System.out.println();
+        System.out.println(weather.weekDay6);
+        System.out.println("High temperature is: " + weather.tempSaturday + "'F");
+        System.out.println("Day will be: " + weather.conditionOfDay2);
+        System.out.println();
+        System.out.println(weather.weekDay7);
+        System.out.println("High temperature is: " + weather.tempSunday + "'F");
+        System.out.println("Day will be: " + weather.conditionOfDay1);
+
+
+    }
+}
---
+++ src/portland_weather/WeeklyForecast.java
@@ -0,0 +1,23 @@
+package portland_weather;
+
+public class WeeklyForecast {
+    double tempMonday = 71.6;
+    int tempTuesday = 81;
+    int tempWednesday = 82;
+    int tempThursday = 85;
+    int tempFriday = 90;
+    int tempSaturday = 94;
+    int tempSunday = 91;
+    String weekDay1 = "Monday, July 10, 2023";
+    String weekDay2 = "Tuesday, July 11, 2023";
+    String weekDay3 = "Wednesday, July 12, 2023";
+    String weekDay4 = "Thursday, July 13, 2023";
+    String weekDay5 = "Friday, July 14, 2023";
+    String weekDay6 = "Saturday, July 15, 2023";
+    String weekDay7 = "Sunday, July 16, 2023";
+    String conditionOfDay1 = "Sunny";
+    String conditionOfDay2 = "Mostly Sunny";
+    String conditionOfDay3 = "Partly Sunny";
+
+
+}
