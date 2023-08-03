68f9fc6de86b71563c45ba699e6a82a0ae1380ad
Lena Zimin <Bogypesn@gmail.com>
2023-08-02 20:00:59 -0700

    hw_july26

--- src/hw_july26/Main.java
+++ src/hw_july26/Main.java
@@ -5,19 +5,19 @@ public class Main {
         WorldState worldState = new WorldState();
         boolean isWorldNegative = false;
         boolean isWorldPositive = true;
-        System.out.println("Is our world is negative when we have wars? "+worldState.isWar);
-        System.out.println("Is our world is negative when we have thefts? "+worldState.isTheft);
-        System.out.println("Is our world is negative when we have cataclysms? "+worldState.isCataclysm);
-        System.out.println("If world is negative am I alive? " +worldState.amIAlive);
-        System.out.println("If world is negative am I hungry? "+worldState.amIHungry);
-        System.out.println("If world is negative am I homeless? "+worldState.anIHomeless);
-        System.out.println("If world is negative our earth collapsed yet? "+worldState.isEarthCollapsed);
-
-        System.out.println("Our world is still at the good state " + isWorldNegativelyGood(worldState.isWar, worldState.isCataclysm, worldState.isTheft, worldState.amIAlive));
-        System.out.println(isWorldPositive);
-
-        System.out.println("World is negative because EARTH COLLAPSED... "+isWorldPositivelyGood(worldState.isWar, worldState.amIHungry, worldState.anIHomeless, worldState.isEarthCollapsed));
-        System.out.println(isWorldNegative);
+        System.out.println("1: Is our world is negative when we have wars? "+worldState.isWar);
+        System.out.println("2: Is our world is negative when we have thefts? "+worldState.isTheft);
+        System.out.println("3: Is our world is negative when we have cataclysms? "+worldState.isCataclysm);
+        System.out.println("4: If world is negative am I alive? " +worldState.amIAlive);
+        System.out.println("5: If world is negative am I hungry? "+worldState.amIHungry);
+        System.out.println("6: If world is negative am I homeless? "+worldState.anIHomeless);
+        System.out.println("7: If world is negative our earth collapsed yet? "+worldState.isEarthCollapsed);
+        System.out.println();
+        System.out.println("Is our world at good state? " + isWorldNegativelyGood(worldState.isWar, worldState.isCataclysm, worldState.isTheft, worldState.amIAlive));
+        System.out.println("After the survey, I still can survive but need changes to improve living? "+isWorldPositive);
+        System.out.println();
+        System.out.println("Is EARTH COLLAPSED? "+isWorldPositivelyGood(worldState.isWar, worldState.amIHungry, worldState.anIHomeless, worldState.isEarthCollapsed));
+        System.out.println("In conclusion, is our world negative? "+isWorldNegative);
     }
     public static boolean isWorldNegativelyGood(boolean isWar, boolean isCataclysm, boolean isTheft, boolean amIAlive) {
         boolean b = !amIAlive;
